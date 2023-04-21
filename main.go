37d0f1c4140abf5285ebe97c49a7ef5598381b1a
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-04-20 22:51:52 -0400

    available/remaining tickets

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import "fmt"
 
 func main() {
 
-	var confName = "IRCTC"
+	var confName = "IPL CSK tickets"
+
+	const confTickets = 40000
+
+	var remainingTickets = 40000
 
 	fmt.Println("Welcome to", confName, "booking")
+	fmt.Println("we have a total of", confTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("GET YOUR TICKETS HERE")
 
 }
