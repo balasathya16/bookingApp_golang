110acbe026db2b454dd2c9d4f37c48322b72f3bc
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-05-02 23:44:43 -0400

    created for loop, fixed indentations

--- main.go
+++ main.go
@@ -14,32 +14,34 @@ func main() {
 	fmt.Printf("we have a total of %v tickets and %v are still available\n", confTickets, remainingTickets)
 	fmt.Println("GET YOUR TICKETS HERE")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	// get user name
-	fmt.Printf("Enter your first name: ")
-	fmt.Scan(&firstName)
+		// get user name
+		fmt.Printf("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Printf("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Printf("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Printf("Enter your email: ")
-	fmt.Scan(&email)
+		fmt.Printf("Enter your email: ")
+		fmt.Scan(&email)
 
-	fmt.Printf("Enter the number of tickets you want: ")
-	fmt.Scan(&userTickets)
+		fmt.Printf("Enter the number of tickets you want: ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
+		remainingTickets = remainingTickets - userTickets
 
-	booking = append(booking, firstName+" "+lastName)
+		booking = append(booking, firstName+" "+lastName)
 
-	fmt.Printf("Thanks %v %v for booking %v tickets. Confirmation email has been sent to %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("Thanks %v %v for booking %v tickets. Confirmation email has been sent to %v\n", firstName, lastName, userTickets, email)
 
-	fmt.Printf("%v %v tickets are currently remaining\n", remainingTickets, confName)
+		fmt.Printf("%v %v tickets are currently remaining\n", remainingTickets, confName)
 
-	fmt.Printf("these are our current bookings %v", booking)
+		fmt.Printf("these are our current bookings %v", booking)
+	}
 
 }
