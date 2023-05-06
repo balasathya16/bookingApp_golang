7bff68e9b954119eaaa60feafe67e502d292e422
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-05-05 22:14:23 -0400

    alternate assigning of vars

--- main.go
+++ main.go
@@ -53,7 +53,8 @@ func main() {
 
 		fmt.Printf("the first names of bookings are %v\n", firstNames)
 
-		if remainingTickets == 0 {
+		noRemainingTickets := remainingTickets == 0
+		if noRemainingTickets {
 			// end for loop
 
 			fmt.Println("Tickets are sold out")
