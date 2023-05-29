f048ae418d167f40edf2c3d59bbb7e09cd2f5707
Bala subramaniam <bala.subramaniam@arcticwolf.com>
2023-05-29 18:19:01 -0400

    passed validation in input params

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	for {
 
 		firstName, lastName, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidTicket := helper.ValidateUserInput(firstName, lastName, email, userTickets)
+		isValidName, isValidEmail, isValidTicket := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
 		if isValidName && isValidEmail && isValidTicket {
 
